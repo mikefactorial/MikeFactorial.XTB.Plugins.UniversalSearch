69cef2533c05c622148e159a2a0b3db5ae84d89c
Michael Ochs <mike@empowrsolutions.com>
2018-11-28 09:56:20 -0500

    Add project files.

---
+++ SDV.cs
@@ -0,0 +1,263 @@
+﻿using Futurez.XrmToolBox.Controls;
+using McTools.Xrm.Connection;
+using Microsoft.Xrm.Sdk;
+using Microsoft.Xrm.Sdk.Messages;
+using Microsoft.Xrm.Sdk.Metadata;
+using Microsoft.Xrm.Sdk.Query;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Windows.Forms;
+using XrmToolBox.Extensibility;
+using XrmToolBox.Extensibility.Args;
+using XrmToolBox.Extensibility.Interfaces;
+
+namespace MikeFactorial.XTB.Plugins
+{
+    public partial class GlobalSearch : PluginControlBase, IStatusBarMessenger, IMessageBusHost, IAboutPlugin, IGitHubPlugin, IHelpPlugin
+    {
+        public string RepositoryName => "D365SatSamples";
+
+        public string UserName => "mikefactorial";
+
+        public string HelpUrl => "https://www.mikefactorial.com/";
+
+        public GlobalSearch()
+        {
+            InitializeComponent();
+            EntitiesListViewControl1.Initialize(this, Service);
+        }
+
+        public event EventHandler<MessageBusEventArgs> OnOutgoingMessage;
+        public event EventHandler<StatusBarMessageEventArgs> SendMessageToStatusBar;
+        delegate void AddTabCallback(EntityCollection collection);
+        /// <summary>
+        /// This event occurs when the connection has been updated in XrmToolBox
+        /// </summary>
+        public override void UpdateConnection(IOrganizationService newService, ConnectionDetail detail, string actionName, object parameter)
+        {
+            base.UpdateConnection(newService, detail, actionName, parameter);
+            foreach (TabPage tabPage in this.tabControl1.TabPages)
+            {
+                foreach (Cinteros.Xrm.CRMWinForm.CRMGridView view in tabPage.Controls)
+                {
+                    view.OrganizationService = detail.ServiceClient;
+                }
+            }
+            EntitiesListViewControl1.Service = detail.ServiceClient;
+        }
+
+        private void LoadData(List<EntityMetadata> selectedEntities)
+        {
+            this.tabControl1.TabPages.Clear();
+            WorkAsync(new WorkAsyncInfo
+            {
+                IsCancelable = true,
+                Message = "Searching...",
+                AsyncArgument = selectedEntities,
+                Work = (worker, args) =>
+                {
+                    selectedEntities = selectedEntities.OrderBy(e => e.LogicalName).ToList();
+                    for (int i = 0; i < selectedEntities.Count; i++)
+                    {
+                        EntityMetadata selectedEntity = selectedEntities[i];
+                        double percentage = (double)(i + 1) / (double)selectedEntities.Count;
+                        percentage = percentage * (double)100;
+
+                        worker.ReportProgress(Convert.ToInt32(percentage), $"Searching for {selectedEntity.LogicalName} records.");
+                        // Retrieve the attribute metadata
+                        var req = new RetrieveEntityRequest()
+                        {
+                            LogicalName = selectedEntity.LogicalName,
+                            EntityFilters = EntityFilters.Attributes,
+                            RetrieveAsIfPublished = true
+                        };
+                        var resp = (RetrieveEntityResponse)this.Service.Execute(req);
+
+                        List<AttributeMetadata> attsToSearch = resp.EntityMetadata.Attributes.Where(meta => (meta.AttributeType == AttributeTypeCode.String || meta.AttributeType == AttributeTypeCode.Memo) && meta.IsValidForRead.Value && meta.IsSearchable.Value).ToList();
+                        Guid guidValue;
+                        if (Guid.TryParse(this.searchTextBox.Text, out guidValue))
+                        {
+                            attsToSearch.AddRange(resp.EntityMetadata.Attributes.Where(meta => (meta.AttributeType == AttributeTypeCode.Lookup || meta.AttributeType == AttributeTypeCode.Owner || meta.AttributeType == AttributeTypeCode.Uniqueidentifier || meta.AttributeType == AttributeTypeCode.Customer) && meta.IsValidForRead.Value && meta.IsSearchable.Value).ToList());
+                        }
+
+                        long intValue;
+                        if (long.TryParse(this.searchTextBox.Text, out intValue))
+                        {
+                            attsToSearch.AddRange(resp.EntityMetadata.Attributes.Where(meta => (meta.AttributeType == AttributeTypeCode.BigInt || meta.AttributeType == AttributeTypeCode.Integer || meta.AttributeType.Value == AttributeTypeCode.Picklist || meta.AttributeType.Value == AttributeTypeCode.State || meta.AttributeType.Value == AttributeTypeCode.Status) && meta.IsValidForRead.Value && meta.IsSearchable.Value).ToList());
+                        }
+
+                        double doubleValue;
+                        if (double.TryParse(this.searchTextBox.Text, out doubleValue))
+                        {
+                            attsToSearch.AddRange(resp.EntityMetadata.Attributes.Where(meta => (meta.AttributeType == AttributeTypeCode.Money || meta.AttributeType == AttributeTypeCode.Decimal || meta.AttributeType.Value == AttributeTypeCode.Double) && meta.IsValidForRead.Value && meta.IsSearchable.Value).ToList());
+                        }
+                        QueryExpression query = new QueryExpression(selectedEntity.LogicalName);
+                        query.ColumnSet = new ColumnSet();
+                        FilterExpression fe = new FilterExpression(LogicalOperator.Or);
+                        query.ColumnSet.AddColumn(selectedEntity.PrimaryIdAttribute);
+                        AttributeMetadata primaryNameAttribute = resp.EntityMetadata.Attributes.FirstOrDefault(a => a.LogicalName == selectedEntity.PrimaryNameAttribute);
+                        if (primaryNameAttribute != null)
+                        {
+                            if (primaryNameAttribute.IsValidForRead.Value && primaryNameAttribute.IsSearchable.Value)
+                            {
+                                query.ColumnSet.AddColumn(selectedEntity.PrimaryNameAttribute);
+                            }
+                            if (primaryNameAttribute.IsSearchable.Value)
+                            {
+                                fe.AddCondition(selectedEntity.PrimaryNameAttribute, ConditionOperator.Like, this.searchTextBox.Text.Replace("*", "%"));
+                            }
+                        }
+                        if (guidValue != Guid.Empty)
+                        {
+                            fe.AddCondition(selectedEntity.PrimaryIdAttribute, ConditionOperator.Equal, guidValue);
+                        }
+                        attsToSearch = attsToSearch.OrderBy(a => a.LogicalName).ToList();
+                        foreach (AttributeMetadata att in attsToSearch)
+                        {
+                            if (att.LogicalName != selectedEntity.PrimaryNameAttribute && att.LogicalName != selectedEntity.PrimaryIdAttribute)
+                            {
+                                query.ColumnSet.AddColumn(att.LogicalName);
+                                if (att.AttributeType == AttributeTypeCode.Lookup || att.AttributeType == AttributeTypeCode.Owner || att.AttributeType == AttributeTypeCode.Uniqueidentifier || att.AttributeType == AttributeTypeCode.Customer)
+                                {
+                                    fe.AddCondition(att.LogicalName, ConditionOperator.Equal, guidValue);
+                                }
+                                else if (att.AttributeType == AttributeTypeCode.BigInt)
+                                {
+                                    fe.AddCondition(att.LogicalName, ConditionOperator.Equal, intValue);
+                                }
+                                else if (att.AttributeType == AttributeTypeCode.Integer || att.AttributeType.Value == AttributeTypeCode.Picklist || att.AttributeType.Value == AttributeTypeCode.State || att.AttributeType.Value == AttributeTypeCode.Status)
+                                {
+                                    fe.AddCondition(att.LogicalName, ConditionOperator.Equal, (Int32)intValue);
+                                }
+                                else if (att.AttributeType.Value == AttributeTypeCode.Double)
+                                {
+                                    fe.AddCondition(att.LogicalName, ConditionOperator.Equal, doubleValue);
+                                }
+                                else if (att.AttributeType == AttributeTypeCode.Money || att.AttributeType == AttributeTypeCode.Decimal)
+                                {
+                                    fe.AddCondition(att.LogicalName, ConditionOperator.Equal, (decimal)doubleValue);
+                                }
+                                else
+                                {
+                                    fe.AddCondition(att.LogicalName, ConditionOperator.Like, this.searchTextBox.Text.Replace("*", "%"));
+                                }
+                            }
+                        }
+                        query.Criteria.AddFilter(fe);
+                        AddTab(Service.RetrieveMultiple(query));
+
+
+                    }
+                },
+                ProgressChanged = (args) =>
+                {
+                    SetWorkingMessage(args.UserState?.ToString());
+                    SendMessageToStatusBar(this, new StatusBarMessageEventArgs(args.ProgressPercentage, args.UserState.ToString().Replace("\r\n", " ")));
+                },
+                PostWorkCallBack = (args) =>
+                {
+                    if (args.Error != null)
+                    {
+                        MessageBox.Show(args.Error.Message, "Oh crap", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        return;
+                    }
+                }
+            });
+        }
+
+        private void AddTab(EntityCollection collection)
+        {
+            if (this.tabControl1.InvokeRequired)
+            {
+                AddTabCallback d = new AddTabCallback(AddTab);
+                this.Invoke(d, new object[] { collection });
+            }
+            else
+            {
+                if (collection.Entities.Count > 0)
+                {
+                    Cinteros.Xrm.CRMWinForm.CRMGridView gridData = new Cinteros.Xrm.CRMWinForm.CRMGridView();
+                    gridData.AllowUserToAddRows = false;
+                    gridData.AllowUserToDeleteRows = false;
+                    gridData.AllowUserToOrderColumns = true;
+                    gridData.AllowUserToResizeRows = false;
+                    gridData.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.AllCells;
+                    gridData.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+                    gridData.Dock = System.Windows.Forms.DockStyle.Fill;
+                    gridData.EntityReferenceClickable = true;
+                    gridData.FilterColumns = "";
+                    gridData.Location = new System.Drawing.Point(8, 38);
+                    gridData.Margin = new System.Windows.Forms.Padding(8, 7, 8, 7);
+                    gridData.Name = "gridData";
+                    gridData.ReadOnly = true;
+                    gridData.RowHeadersVisible = false;
+                    gridData.ShowIndexColumn = false;
+                    gridData.Size = new System.Drawing.Size(1587, 1090);
+                    gridData.TabIndex = 0;
+                    gridData.RecordClick += new Cinteros.Xrm.CRMWinForm.CRMRecordEventHandler(gridData_RecordClick);
+                    gridData.RecordDoubleClick += new Cinteros.Xrm.CRMWinForm.CRMRecordEventHandler(this.gridData_RecordDoubleClick);
+                    gridData.DataSource = collection;
+                    TabPage tabPage = new TabPage(collection.EntityName);
+                    tabPage.Controls.Add(gridData);
+
+                    this.tabControl1.TabPages.Add(tabPage);
+                }
+            }
+        }
+        private void OpenEntityReference(EntityReference entref)
+        {
+            if (!string.IsNullOrEmpty(entref.LogicalName) && !entref.Id.Equals(Guid.Empty))
+            {
+                var url = ConnectionDetail.WebApplicationUrl;
+                if (string.IsNullOrEmpty(url))
+                {
+                    url = string.Concat(ConnectionDetail.ServerName, "/", ConnectionDetail.Organization);
+                    if (!url.ToLower().StartsWith("http"))
+                    {
+                        url = string.Concat("http://", url);
+                    }
+                }
+                url = string.Concat(url,
+                    url.EndsWith("/") ? "" : "/",
+                    "main.aspx?etn=",
+                    entref.LogicalName,
+                    "&pagetype=entityrecord&id=",
+                    entref.Id.ToString());
+                if (!string.IsNullOrEmpty(url))
+                {
+                    Process.Start(url);
+                }
+            }
+        }
+
+        private void gridData_RecordDoubleClick(object sender, Cinteros.Xrm.CRMWinForm.CRMRecordEventArgs e)
+        {
+            OpenEntityReference(e.Entity.ToEntityReference());
+        }
+
+        private void gridData_RecordClick(object sender, Cinteros.Xrm.CRMWinForm.CRMRecordEventArgs e)
+        {
+            if (e.Value is EntityReference entref)
+            {
+                OpenEntityReference(entref);
+            }
+        }
+
+        public void OnIncomingMessage(MessageBusEventArgs message)
+        {
+            MessageBox.Show("Simple Data Viewer cannot yet handle calls from other tools.");
+        }
+
+        public void ShowAboutDialog()
+        {
+            MessageBox.Show("Simple Data Viewer\n\nD365 Saturday Sample project");
+        }
+
+        private void btnFind_Click(object sender, EventArgs e)
+        {
+            LoadData(this.EntitiesListViewControl1.CheckedEntities);
+        }
+    }
+}
---
+++ SDV.designer.cs
@@ -0,0 +1,180 @@
+﻿namespace MikeFactorial.XTB.Plugins
+{
+    partial class GlobalSearch
+    {
+        /// <summary> 
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur de composants
+
+        /// <summary> 
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas 
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(GlobalSearch));
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.EntitiesListViewControl1 = new Futurez.XrmToolBox.Controls.EntitiesListControl();
+            this.groupBox4 = new System.Windows.Forms.GroupBox();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.searchTextBox = new System.Windows.Forms.TextBox();
+            this.btnFind = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.groupBox4.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.groupBox1);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.groupBox4);
+            this.splitContainer1.Panel2.Controls.Add(this.groupBox3);
+            this.splitContainer1.Size = new System.Drawing.Size(831, 533);
+            this.splitContainer1.SplitterDistance = 225;
+            this.splitContainer1.TabIndex = 5;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.EntitiesListViewControl1);
+            this.groupBox1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.groupBox1.Location = new System.Drawing.Point(0, 0);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(225, 523);
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Entity";
+            // 
+            // EntitiesListViewControl1
+            // 
+            this.EntitiesListViewControl1.AutoLoadData = true;
+            this.EntitiesListViewControl1.Checkboxes = true;
+            this.EntitiesListViewControl1.ColumnDisplayMode = Futurez.XrmToolBox.Controls.ListViewColumnDisplayMode.Compact;
+            this.EntitiesListViewControl1.DisplayToolbar = true;
+            this.EntitiesListViewControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.EntitiesListViewControl1.EntityTypes = Futurez.XrmToolBox.Controls.EnumEntityTypes.BothCustomAndSystem;
+            this.EntitiesListViewControl1.GroupByType = false;
+            this.EntitiesListViewControl1.Location = new System.Drawing.Point(3, 16);
+            this.EntitiesListViewControl1.Margin = new System.Windows.Forms.Padding(8, 7, 8, 7);
+            this.EntitiesListViewControl1.Name = "EntitiesListViewControl1";
+            this.EntitiesListViewControl1.ParentBaseControl = this;
+            this.EntitiesListViewControl1.RetrieveAsIfPublished = true;
+            this.EntitiesListViewControl1.Service = null;
+            this.EntitiesListViewControl1.Size = new System.Drawing.Size(219, 504);
+            this.EntitiesListViewControl1.TabIndex = 1;
+            // 
+            // groupBox4
+            // 
+            this.groupBox4.Controls.Add(this.tabControl1);
+            this.groupBox4.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.groupBox4.Location = new System.Drawing.Point(0, 57);
+            this.groupBox4.Name = "groupBox4";
+            this.groupBox4.Size = new System.Drawing.Size(602, 476);
+            this.groupBox4.TabIndex = 3;
+            this.groupBox4.TabStop = false;
+            this.groupBox4.Text = "Records";
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tabControl1.Location = new System.Drawing.Point(3, 16);
+            this.tabControl1.Margin = new System.Windows.Forms.Padding(1, 1, 1, 1);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(596, 457);
+            this.tabControl1.TabIndex = 0;
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.searchTextBox);
+            this.groupBox3.Controls.Add(this.btnFind);
+            this.groupBox3.Dock = System.Windows.Forms.DockStyle.Top;
+            this.groupBox3.Location = new System.Drawing.Point(0, 0);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(602, 57);
+            this.groupBox3.TabIndex = 2;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Options";
+            // 
+            // searchTextBox
+            // 
+            this.searchTextBox.Location = new System.Drawing.Point(4, 27);
+            this.searchTextBox.Margin = new System.Windows.Forms.Padding(1, 1, 1, 1);
+            this.searchTextBox.Name = "searchTextBox";
+            this.searchTextBox.Size = new System.Drawing.Size(517, 20);
+            this.searchTextBox.TabIndex = 5;
+            // 
+            // btnFind
+            // 
+            this.btnFind.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnFind.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnFind.Location = new System.Drawing.Point(525, 24);
+            this.btnFind.Name = "btnFind";
+            this.btnFind.Size = new System.Drawing.Size(71, 23);
+            this.btnFind.TabIndex = 4;
+            this.btnFind.Text = "Search";
+            this.btnFind.UseVisualStyleBackColor = true;
+            this.btnFind.Click += new System.EventHandler(this.btnFind_Click);
+            // 
+            // GlobalSearch
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.splitContainer1);
+            this.Name = "GlobalSearch";
+            this.PluginIcon = ((System.Drawing.Icon)(resources.GetObject("$this.PluginIcon")));
+            this.Size = new System.Drawing.Size(831, 533);
+            this.TabIcon = ((System.Drawing.Image)(resources.GetObject("$this.TabIcon")));
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox4.ResumeLayout(false);
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private Futurez.XrmToolBox.Controls.EntitiesListControl EntitiesListViewControl1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox4;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.Button btnFind;
+        private System.Windows.Forms.TextBox searchTextBox;
+        private System.Windows.Forms.TabControl tabControl1;
+    }
+}
